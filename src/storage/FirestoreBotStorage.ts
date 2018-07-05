a3ca0137bb794701b4dc078912c56568cf970cbe
Frank Maker <fmaker@studybotz.com>
2018-07-05 21:56:40 +0200

    Didnt get this to work

--- src/storage/FirestoreBotStorage.ts
+++ src/storage/FirestoreBotStorage.ts
@@ -1,3 +1,4 @@
+const util = require('util')
 
 import { BotStorage } from "./BotStorage";
 import {
@@ -5,22 +6,21 @@ import {
     IFirestoreBotStorageSettings,
 } from "../types";
 
-
-function toDatastore(obj: any, nonIndexed: any) {
-    nonIndexed = nonIndexed || [];
-    const results: any = [];
-    Object.keys(obj).forEach((k) => {
-        if (obj[k] === undefined) {
-            return;
-        }
-        results.push({
-            name: k,
-            value: obj[k],
-            excludeFromIndexes: nonIndexed.indexOf(k) !== -1
-        });
-    });
-    return results;
-}
+// function toDatastore(obj: any, nonIndexed: any) {
+//     nonIndexed = nonIndexed || [];
+//     const results: any = [];
+//     Object.keys(obj).forEach((k) => {
+//         if (obj[k] === undefined) {
+//             return;
+//         }
+//         results.push({
+//             name: k,
+//             value: obj[k],
+//             excludeFromIndexes: nonIndexed.indexOf(k) !== -1
+//         });
+//     });
+//     return results;
+// }
 
 export class FirestoreBotStorage extends BotStorage {
     constructor(collection: any, public settings: IFirestoreBotStorageSettings) {
@@ -29,91 +29,63 @@ export class FirestoreBotStorage extends BotStorage {
         if (!collection) { // || (typeof (kind) === "undefined")) {
             throw new Error("Invalid constructor arguments for the FirestoreBotStorage class. FirestoreBotStorage");
         }
-        // function fromDatastore(obj) {
-        //     obj.id = obj[collection.KEY].id;
-        //     return obj;
-        // }
+
+        const firestore = collection.firestore
         this.getDataFunction = (data: IBotStorageDataHash, entry: any, resolve: any, reject: any) => {
 
-            // let q = collection.createQuery(kind).filter('key', '=', entry.key)
-            // collection.runQuery(q, (err, entities, nextQuery) => {
-            let q = collection.where('key', '==', entry.key).get()
-                .then(querySnapshot => {
-                    const docs = querySnapshot.docs.map(doc => doc.data())
-                    let item = docs[0]
-
-                    // let item = entities.map(fromDatastore)[0];
-                    // console.log("item", item)
-
-                    var docData = "{}";
-                    var hash;
-                    if (item) {
-                        docData = item.data;
-                        hash = item.hash;
-                    }
-                    var hashKey = entry.type + "Hash";
-                    data[entry.type] = JSON.parse(docData);
-                    data[hashKey] = hash;
-                    resolve();
+            // Store data into firestore using entry key as id
+            firestore.runTransaction(trans => {
+                const docRef = collection.doc(entry.key)
+                return trans.get(docRef)
+                    .then(doc => {
+                        data = doc.data()
+                        console.debug("GET KEY: ", entry.key, "DATA: ", data)
+                        return Promise.resolve()
+                    })
+            })
+                .then(result => {
+                    // console.log("Transaction success!")
+                    resolve()
                 })
                 .catch(err => {
-                    reject(err);
-                    return;
+                    console.log("Transaction failure: ", err)
+                    reject()
                 })
 
         }
 
 
         this.saveDataFunction = ((entry: any, resolve: any, reject: any) => {
-            // const { key, data, hash, type, lastModified, expireAt } = entry;
-            const firestore = collection.firestore
-            console.debug("RUN TRANSACTION")
+            const { key, data, hash, type, lastModified, expireAt } = entry;
+            const dataObj = {
+                state: JSON.stringify({
+                    data,
+                    hash,
+                    type,
+                    lastModified,
+                    expireAt
+                }),
+                key
+            }
+
+            console.debug("SAVE KEY: ", key, " DATA (", typeof dataObj, "): ", JSON.stringify(dataObj))
+
+            // Store data into firestore using entry key as id
             firestore.runTransaction(trans => {
-                console.debug("Storing entry with key: ", entry.key)
-                console.debug("Entry: ", entry)
-                const doc = collection.doc(entry.key)
-                trans.set(doc, entry)
+                let dataObj: IBotStorageDataHash
+
+                const doc = collection.doc(key)
+                trans.set(doc, dataObj)
                 return Promise.resolve()
             })
                 .then(result => {
-                    console.log("Transaction success!")
+                    // console.log("Transaction success!")
+                    resolve()
                 })
                 .catch(err => {
                     console.log("Transaction failure: ", err)
+                    reject()
                 })
-
-            // let q = collection.createQuery(kind).filter('key', '=', entry.key)
-            // ds.runQuery(q, (err, entities, nextQuery) => {
-            //     if (err) {
-            //         reject(err);
-            //         return;
-            //     }
-            //     // console.log("item.id", item)
-            //     let taskEntity = {
-            //         key: ds.key(kind),
-            //         data: {
-            //             key,
-            //             data,
-            //             hash,
-            //             type,
-            //             lastModified,
-            //             expireAt
-            //         },
-            //     };
-            //     let item = entities.map(fromDatastore)[0];
-
-            //     if (item) {
-            //         taskEntity.key = ds.key([kind, parseInt(item.id, 10)]);
-            //     }
-
-            //     ds.save(taskEntity, (err) => {
-            //         if (err) {
-            //             reject(err);
-            //             return;
-            //         }
-            //         resolve()
-            //     });
-
 
         })
     }
